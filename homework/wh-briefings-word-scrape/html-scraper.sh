29aef98556a021901fbe99ccb9138f12fc4e57a7
Allison McCartney <allison.n.mccartney@gmail.com>
2015-01-25 20:16:24 -0800

    Update html-scraper.sh

--- homework/wh-briefings-word-scrape/html-scraper.sh
+++ homework/wh-briefings-word-scrape/html-scraper.sh
@@ -2,7 +2,11 @@
 
 	#strips all html and counts words in all White House briefings
 
-	cat data-hold/* | ~/bin_compciv/pup 'div#content' | sed -e :a -e 's/<[^>]*>//g;/</N;//ba' | sed "s/&#39;/'/g" | grep -oE '[[:alpha:]]{7,}' | sort | uniq -c | sort -nr | head  -10 > topten.txt
+	cat data-hold/* | ~/bin_compciv/pup 'div#content' | \
+	  sed -e :a -e 's/<[^>]*>//g;/</N;//ba' | \
+	  sed "s/&#39;/'/g" | \
+	  grep -oE '[[:alpha:]]{7,}' | \
+	  sort | uniq -c | sort -nr | head  -10 > topten.txt
 
 
 
