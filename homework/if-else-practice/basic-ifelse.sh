1ceaa691a966794296ec672cdaa71d8b7430bc39
amccartney <amccartn@corn22.stanford.edu>
2015-02-03 11:51:42 -0800

    typo

--- homework/if-else-practice/basic-ifelse.sh
+++ homework/if-else-practice/basic-ifelse.sh
@@ -3,42 +3,43 @@
 if [[ "$#" -lt 1 ]]; then
   echo "You need at least one argument"
 
-elif [[ "$#" -eq 1 ]]; then
-# just one argument
-  echo "There is just one argument: $1"
+  elif [[ "$#" -eq 1 ]]; then
+    echo "There is just one argument: $1"
 
-elif [[ "$#" -eq 2 ]]; then
+  elif [[ "$#" -eq 2 ]]; then
+    echo "There are two arguments: $1 and $2"
 
-  echo "There are two arguments: $1 and $2"
+    if [[ $1 -eq $2 ]]; then
+      echo "$1 is equal to $2"
 
-  if [[ $1 -eq $2 ]]; then
-    echo "$1 is equal to $2"
+    elif [[ $1 -gt $2 ]]; then
+      echo "$1 is greater than $2"
 
-  elif [[ $1 -gt $2 ]]; then
-    echo "$1 is greater than $2"
-
-  elif [[ $1 -lt $2 ]]; then
-    echo "$1 is less than $2"
+    elif [[ $1 -lt $2 ]]; then
+      echo "$1 is less than $2"
   fi
 
-elif [[ "$#" -eq 3 ]]; then
+  elif [[ "$#" -eq 3 ]]; then
+    echo "There are three arguments: $1, $2, and $3"
 
-  echo "There are three arguments: $1, $2, and $3"
+    if [[ $1 -eq $2 ]]; then
+      str_a="is equal to $2"
 
-  if [[ $1 -eq $2 ]]; then
-    str_a="is equal to $2"
-  elif [[ $1 -lt $2 ]]; then
-    str_a="is less than $2"
-  elif [[ $1 -gt $2 ]]; then
-  	str_a="is greater than $2"
+    elif [[ $1 -lt $2 ]]; then
+      str_a="is less than $2"
+
+    elif [[ $1 -gt $2 ]]; then
+      str_a="is greater than $2"
   fi
 
   if [[ $1 -eq $3 ]]; then
     str_b="is equal to $3"
+
   elif [[ $1 -lt $3 ]]; then
     str_b="is less than $3"
+
   elif [[ $1 -gt $3 ]]; then
-  	str_b="is greater than $3
+    str_b="is greater than $3"
   fi
 
   echo "$1 $str_a and $str_b"
@@ -46,4 +47,3 @@ elif [[ "$#" -eq 3 ]]; then
 elif [[ "$#" -gt 3 ]]; then
   echo "You need fewer than $# arguments"
 fi
-
