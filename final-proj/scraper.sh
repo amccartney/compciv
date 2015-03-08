38d3834ffb3d645ea4d64a5e28d4cc900f3c3e57
amccartney <amccartn@corn22.stanford.edu>
2015-03-08 14:51:51 -0700

    what

--- final-proj/scraper.sh
+++ final-proj/scraper.sh
@@ -1,20 +1,33 @@
 mkdir -p data-hold/press-releases
+mkdir -p data-hold/menu-pages
 
+# curl the menu pages of Cruz's press releases
 for num in $(seq 1 30); do
-  curl -s "http://www.cruz.senate.gov/index.cfm?p=press_releases&pg=$num" > data-hold/$num.html
+  curl -s "http://www.cruz.senate.gov/index.cfm?p=press_releases&pg=$num" > data-hold/menu-pages/$num.html
 done
 
-cat data-hold/*.html | \
+# extract the relative urls of each press release from the menu pages
+cat data-hold/menu-pages/*.html | \
   pup 'a attr{href}' | \
   grep -v 'index' | \
-  grep 'press_release' > data-hold/rel-urls.txt
+  grep 'press_release&' | \
+  sed 's/amp;//' | \
+  uniq > data-hold/rel-urls.txt
 
+rm -rf menu-pages
+
+# curl each relative url and saves it into an html file with the press releases id number
 urls=$(cat data-hold/rel-urls.txt)
 
 for line in $urls; do
-  fname=$(echo $line | cut -c 26-)
-  newLine=$(echo $line | sed 's/amp;//')
-  curl -s "http://www.cruz.senate.gov$newLine" > data-hold/press-releases/$fname.html
-  echo "Curling http://www.cruz.senate.gov$newLine ..."
-  sleep 3
+
+  fname=$(echo $line | cut -c 22-)
+
+  curl -s "http://www.cruz.senate.gov$line" | \
+  pup 'div#main' | \
+  sed -e :a -e 's/<[^>]*>//g;/</N;//ba' | \
+  sed 's/&#34;/"/g' | \
+  sed "s/&#39;/'/g" > data-hold/press-releases/$fname.txt
+  echo "Curling http://www.cruz.senate.gov$line ..."
+  sleep 2
 done
